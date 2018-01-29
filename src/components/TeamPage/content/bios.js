f4692903f25870b45f67bd5086977eafeee68c11
Colin Williams <hello@colinwilliams.org>
2018-01-28 23:38:18 -0800

    Team page basic layout

---
+++ src/components/TeamPage/content/bios.js
@@ -0,0 +1,271 @@
+export const en = {
+  title: 'Skycoin Team',
+  description: 'Bios of the Skycoin team',
+  heading: '',
+  bios: [
+    {
+      name: 'Brandon',
+      picture: '',
+      title: 'Founder',
+      bio: 'Brandon is one of the original developers behind Bitcoin. He started Skycoin 8 years ago with a vision of creating a new, decentralized Internet. Brandon sits on the advisory board of several cryptocurrency projects. His background is mathematics, distributed systems, and symbolic logic.',
+    },
+    {
+      name: 'Steve',
+      picture: '',
+      title: 'CTO',
+      bio: 'Steve has a background in ...',
+    },
+    {
+      name: 'Bradford',
+      picture: 'bradford.jpg',
+      title: 'COO-West',
+      bio: 'Bradford was a C-level executive at five VC-backed companies. He is the creator of Spire, one of the first distributed SQL databases. Besides Big Data, other companies he assisted were in fashion, security, and adtech. His teams also drove several exits, including one public M&A. He holds a degree in Computer Science and economics, specializing in distributed systems.',
+    },
+    {
+      name: 'Catherine',
+      picture: '',
+      title: 'Director of Marketing',
+      bio: 'Catherine is a marketer with experience in the digital, traditional and affiliate spaces. Before entering marketing, she worked as a journalist on projects which received awards from American City Business Journals and the Associated Press. Catherine holds degrees in Political Science and Communication.',
+    },
+    {
+      name: 'Sam',
+      picture: '',
+      title: '',
+      bio: 'He started blockchain investment since 2014 with a number of successful portfolios. He’s also a founding partner of a management consulting firm with offices in Shanghai and Singapore. Prior to that, He served a Singapore group as global project manager. He also served a German Fortune 500 company as project manager and solution architect. He was responsible for the delivery of a number of system consulting solutions for Fortune 500 companies. He holds a degree from UniSA.',
+    },
+    {
+      name: 'Jesse Sun',
+      picture: '',
+      title: '',
+      bio: 'Jesse is both creator and head of a fund investing in blockchain startups and blockchain applications and cryptocurrencies, as well as bitcoin mining. He took the COO position of one of the first blockchain companies back in 2014. Before joining the crypto world, he was a management consultant. Jesse holds an MBA of Manchester Business School (FT Global Top 30) and a master degree of Tongji University.',
+    },
+    {
+      name: 'Mary Li',
+      picture: '',
+      title: '',
+      bio: 'She is a serial entrepreneur and she started blockchain angel investment since 2015. She also participated in the operation of blockchain startups, responsible for marketing. She founded a blockchain investment fund in 2017, focusing on blockchain startups and digital currency.',
+    },
+    {
+      name: 'Jason Gao',
+      picture: '',
+      title: '',
+      bio: 'Technophile, strategist and marketer, Jason has expertise in both marketing and business strategy. In addition to his work with Skycoin, Jason serves as China CEO of a boutique international consulting firm. Prior to this, he served a series of global companies across professional services industry and media industry. Jason speaks fluent English, Chinese, Japanese and French. He holds a masters degree in advertising & communication from top university in China.',
+    },
+    {
+      name: 'Michael M',
+      picture: '',
+      title: 'Designer',
+      bio: 'Creative direction, design, branding for fortune 500 companies, boutiques, working with the top athletes, talent, photographers, and creative in the world. 5 years at Nike, Apple / Beats by Dr Dre.',
+    },
+    {
+      name: 'Erich Quist',
+      picture: 'erich.jpg',
+      title: 'Director of Digital Experience',
+      bio: '',
+    },
+  ],
+};
+
+export const zh = {
+  title: 'Skycoin Team',
+  description: 'Bios of the Skycoin team',
+  heading: '',
+  bios: [
+    {
+      name: 'Brandon',
+      picture: '',
+      title: 'Founder',
+      bio: 'Brandon is one of the original developers behind Bitcoin. He started Skycoin 8 years ago with a vision of creating a new, decentralized Internet. Brandon sits on the advisory board of several cryptocurrency projects. His background is mathematics, distributed systems, and symbolic logic.',
+    },
+    {
+      name: 'Steve',
+      picture: '',
+      title: 'CTO',
+      bio: 'Steve has a background in ...',
+    },
+    {
+      name: 'Bradford',
+      picture: 'bradford.jpg',
+      title: 'COO-West',
+      bio: 'Bradford was a C-level executive at five VC-backed companies. He is the creator of Spire, one of the first distributed SQL databases. Besides Big Data, other companies he assisted were in fashion, security, and adtech. His teams also drove several exits, including one public M&A. He holds a degree in Computer Science and economics, specializing in distributed systems.',
+    },
+    {
+      name: 'Catherine',
+      picture: '',
+      title: 'Director of Marketing',
+      bio: 'Catherine is a marketer with experience in the digital, traditional and affiliate spaces. Before entering marketing, she worked as a journalist on projects which received awards from American City Business Journals and the Associated Press. Catherine holds degrees in Political Science and Communication.',
+    },
+    {
+      name: 'Sam',
+      picture: '',
+      title: '',
+      bio: 'He started blockchain investment since 2014 with a number of successful portfolios. He’s also a founding partner of a management consulting firm with offices in Shanghai and Singapore. Prior to that, He served a Singapore group as global project manager. He also served a German Fortune 500 company as project manager and solution architect. He was responsible for the delivery of a number of system consulting solutions for Fortune 500 companies. He holds a degree from UniSA.',
+    },
+    {
+      name: 'Jesse Sun',
+      picture: '',
+      title: '',
+      bio: 'Jesse is both creator and head of a fund investing in blockchain startups and blockchain applications and cryptocurrencies, as well as bitcoin mining. He took the COO position of one of the first blockchain companies back in 2014. Before joining the crypto world, he was a management consultant. Jesse holds an MBA of Manchester Business School (FT Global Top 30) and a master degree of Tongji University.',
+    },
+    {
+      name: 'Mary Li',
+      picture: '',
+      title: '',
+      bio: 'She is a serial entrepreneur and she started blockchain angel investment since 2015. She also participated in the operation of blockchain startups, responsible for marketing. She founded a blockchain investment fund in 2017, focusing on blockchain startups and digital currency.',
+    },
+    {
+      name: 'Jason Gao',
+      picture: '',
+      title: '',
+      bio: 'Technophile, strategist and marketer, Jason has expertise in both marketing and business strategy. In addition to his work with Skycoin, Jason serves as China CEO of a boutique international consulting firm. Prior to this, he served a series of global companies across professional services industry and media industry. Jason speaks fluent English, Chinese, Japanese and French. He holds a masters degree in advertising & communication from top university in China.',
+    },
+    {
+      name: 'Michael M',
+      picture: '',
+      title: 'Designer',
+      bio: 'Creative direction, design, branding for fortune 500 companies, boutiques, working with the top athletes, talent, photographers, and creative in the world. 5 years at Nike, Apple / Beats by Dr Dre.',
+    },
+    {
+      name: 'Erich Quist',
+      picture: 'erich.jpg',
+      title: 'Director of Digital Experience',
+      bio: '',
+    },
+  ],
+};
+
+export const ru = {
+  title: 'Skycoin Team',
+  description: 'Bios of the Skycoin team',
+  heading: '',
+  bios: [
+    {
+      name: 'Brandon',
+      picture: '',
+      title: 'Founder',
+      bio: 'Brandon is one of the original developers behind Bitcoin. He started Skycoin 8 years ago with a vision of creating a new, decentralized Internet. Brandon sits on the advisory board of several cryptocurrency projects. His background is mathematics, distributed systems, and symbolic logic.',
+    },
+    {
+      name: 'Steve',
+      picture: '',
+      title: 'CTO',
+      bio: 'Steve has a background in ...',
+    },
+    {
+      name: 'Bradford',
+      picture: 'bradford.jpg',
+      title: 'COO-West',
+      bio: 'Bradford was a C-level executive at five VC-backed companies. He is the creator of Spire, one of the first distributed SQL databases. Besides Big Data, other companies he assisted were in fashion, security, and adtech. His teams also drove several exits, including one public M&A. He holds a degree in Computer Science and economics, specializing in distributed systems.',
+    },
+    {
+      name: 'Catherine',
+      picture: '',
+      title: 'Director of Marketing',
+      bio: 'Catherine is a marketer with experience in the digital, traditional and affiliate spaces. Before entering marketing, she worked as a journalist on projects which received awards from American City Business Journals and the Associated Press. Catherine holds degrees in Political Science and Communication.',
+    },
+    {
+      name: 'Sam',
+      picture: '',
+      title: '',
+      bio: 'He started blockchain investment since 2014 with a number of successful portfolios. He’s also a founding partner of a management consulting firm with offices in Shanghai and Singapore. Prior to that, He served a Singapore group as global project manager. He also served a German Fortune 500 company as project manager and solution architect. He was responsible for the delivery of a number of system consulting solutions for Fortune 500 companies. He holds a degree from UniSA.',
+    },
+    {
+      name: 'Jesse Sun',
+      picture: '',
+      title: '',
+      bio: 'Jesse is both creator and head of a fund investing in blockchain startups and blockchain applications and cryptocurrencies, as well as bitcoin mining. He took the COO position of one of the first blockchain companies back in 2014. Before joining the crypto world, he was a management consultant. Jesse holds an MBA of Manchester Business School (FT Global Top 30) and a master degree of Tongji University.',
+    },
+    {
+      name: 'Mary Li',
+      picture: '',
+      title: '',
+      bio: 'She is a serial entrepreneur and she started blockchain angel investment since 2015. She also participated in the operation of blockchain startups, responsible for marketing. She founded a blockchain investment fund in 2017, focusing on blockchain startups and digital currency.',
+    },
+    {
+      name: 'Jason Gao',
+      picture: '',
+      title: '',
+      bio: 'Technophile, strategist and marketer, Jason has expertise in both marketing and business strategy. In addition to his work with Skycoin, Jason serves as China CEO of a boutique international consulting firm. Prior to this, he served a series of global companies across professional services industry and media industry. Jason speaks fluent English, Chinese, Japanese and French. He holds a masters degree in advertising & communication from top university in China.',
+    },
+    {
+      name: 'Michael M',
+      picture: '',
+      title: 'Designer',
+      bio: 'Creative direction, design, branding for fortune 500 companies, boutiques, working with the top athletes, talent, photographers, and creative in the world. 5 years at Nike, Apple / Beats by Dr Dre.',
+    },
+    {
+      name: 'Erich Quist',
+      picture: 'erich.jpg',
+      title: 'Director of Digital Experience',
+      bio: '',
+    },
+  ],
+};
+
+export const ko = {
+  title: 'Skycoin Team',
+  description: 'Bios of the Skycoin team',
+  heading: '',
+  bios: [
+    {
+      name: 'Brandon',
+      picture: '',
+      title: 'Founder',
+      bio: 'Brandon is one of the original developers behind Bitcoin. He started Skycoin 8 years ago with a vision of creating a new, decentralized Internet. Brandon sits on the advisory board of several cryptocurrency projects. His background is mathematics, distributed systems, and symbolic logic.',
+    },
+    {
+      name: 'Steve',
+      picture: '',
+      title: 'CTO',
+      bio: 'Steve has a background in ...',
+    },
+    {
+      name: 'Bradford',
+      picture: 'bradford.jpg',
+      title: 'COO-West',
+      bio: 'Bradford was a C-level executive at five VC-backed companies. He is the creator of Spire, one of the first distributed SQL databases. Besides Big Data, other companies he assisted were in fashion, security, and adtech. His teams also drove several exits, including one public M&A. He holds a degree in Computer Science and economics, specializing in distributed systems.',
+    },
+    {
+      name: 'Catherine',
+      picture: '',
+      title: 'Director of Marketing',
+      bio: 'Catherine is a marketer with experience in the digital, traditional and affiliate spaces. Before entering marketing, she worked as a journalist on projects which received awards from American City Business Journals and the Associated Press. Catherine holds degrees in Political Science and Communication.',
+    },
+    {
+      name: 'Sam',
+      picture: '',
+      title: '',
+      bio: 'He started blockchain investment since 2014 with a number of successful portfolios. He’s also a founding partner of a management consulting firm with offices in Shanghai and Singapore. Prior to that, He served a Singapore group as global project manager. He also served a German Fortune 500 company as project manager and solution architect. He was responsible for the delivery of a number of system consulting solutions for Fortune 500 companies. He holds a degree from UniSA.',
+    },
+    {
+      name: 'Jesse Sun',
+      picture: '',
+      title: '',
+      bio: 'Jesse is both creator and head of a fund investing in blockchain startups and blockchain applications and cryptocurrencies, as well as bitcoin mining. He took the COO position of one of the first blockchain companies back in 2014. Before joining the crypto world, he was a management consultant. Jesse holds an MBA of Manchester Business School (FT Global Top 30) and a master degree of Tongji University.',
+    },
+    {
+      name: 'Mary Li',
+      picture: '',
+      title: '',
+      bio: 'She is a serial entrepreneur and she started blockchain angel investment since 2015. She also participated in the operation of blockchain startups, responsible for marketing. She founded a blockchain investment fund in 2017, focusing on blockchain startups and digital currency.',
+    },
+    {
+      name: 'Jason Gao',
+      picture: '',
+      title: '',
+      bio: 'Technophile, strategist and marketer, Jason has expertise in both marketing and business strategy. In addition to his work with Skycoin, Jason serves as China CEO of a boutique international consulting firm. Prior to this, he served a series of global companies across professional services industry and media industry. Jason speaks fluent English, Chinese, Japanese and French. He holds a masters degree in advertising & communication from top university in China.',
+    },
+    {
+      name: 'Michael M',
+      picture: '',
+      title: 'Designer',
+      bio: 'Creative direction, design, branding for fortune 500 companies, boutiques, working with the top athletes, talent, photographers, and creative in the world. 5 years at Nike, Apple / Beats by Dr Dre.',
+    },
+    {
+      name: 'Erich Quist',
+      picture: 'erich.jpg',
+      title: 'Director of Digital Experience',
+      bio: '',
+    },
+  ],
+};
