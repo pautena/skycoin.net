e56cc50137c308963f9a6f64b76e52822dcdb8c4
Colin Williams <hello@colinwilliams.org>
2018-02-14 21:28:42 -0800

    Updating teampage with bios and images

--- src/components/TeamPage/TeamPage.js
+++ src/components/TeamPage/TeamPage.js
@@ -18,10 +18,6 @@ const Wrapper = styled.div`
   padding: 2em 0;
 `;
 
-const Description = styled.strong`
-  font-family: ${FONT_FAMILIES.sansBold};
-`;
-
 const Person = styled.article`
   border-bottom: 1px solid #eceef0;
 
@@ -31,16 +27,11 @@ const Person = styled.article`
 `;
 
 const ImageContainer = styled.div`
-  height: 0;
-  overflow: hidden;
-  padding-top: 100%;
   position: relative;
   width: 100%;
 `;
 
 const Img = styled.img`
-  position: absolute;
-  top: 0;
   width: 100%;
 `;
 
@@ -67,6 +58,10 @@ const StyledBox = styled(Box)`
   flex-direction: column;
 `;
 
+const BioSection = styled.section`
+  margin-bottom: 3em;
+`;
+
 const localeList = { en, zh, ko, ru };
 
 const getLocale = (locale) => {
@@ -89,26 +84,110 @@ const TeamPage = ({ intl }) => (
         <Heading heavy as="h1" fontSize={[5, 6]} color="black" mb={[4, 6]}>
           {getLocale(intl.locale).title}
         </Heading>
-        <Description>{getLocale(intl.locale).description}</Description>
-        {getLocale(intl.locale).bios.west.map(({ name, picture, title, bio }, index) => (
-          <Person key={index}>
-            <Flex wrap py={[7, 8]}>
-              <StyledBox width={[1 / 1, 1 / 4]} pr={[4, 6]}>
-                <ImageContainer>
-                  {picture ?
-                    <Img src={`./content/images/${picture}`} alt={`Picture of ${name}`} /> : // eslint-disable-line global-require
-                    <NoImg />
-                  }
-                </ImageContainer>
-              </StyledBox>
-              <StyledBox width={[1 / 1, 3 / 4]}>
-                <Heading heavy as="h2" fontSize={[3, 4]}>{name}</Heading>
-                <Title>{title}</Title>
-                <Bio>{bio}</Bio>
-              </StyledBox>
-            </Flex>
-          </Person>
-        ))}
+        <BioSection>
+          <Heading as="h2" fontSize={[4, 5]} color="black" mb={[3, 5]}>
+            {getLocale(intl.locale).founders.title}
+          </Heading>
+          {getLocale(intl.locale).founders.bios.map(({ name, picture, title, bio }, index) => (
+            <Person key={index}>
+              <Flex wrap py={[7, 8]}>
+                <StyledBox width={[1 / 1, 1 / 4]} pr={[4, 6]}>
+                  <ImageContainer>
+                    {picture ?
+                      /* eslint-disable */
+                      <Img src={require(`./content/images/${picture}`)} alt={`Picture of ${name}`} /> :
+                      /* eslint-disable */
+                      <NoImg />
+                    }
+                  </ImageContainer>
+                </StyledBox>
+                <StyledBox width={[1 / 1, 3 / 4]}>
+                  <Heading heavy as="h2" fontSize={[3, 4]}>{name}</Heading>
+                  <Title>{title}</Title>
+                  <Bio>{bio}</Bio>
+                </StyledBox>
+              </Flex>
+            </Person>
+          ))}
+        </BioSection>
+        <BioSection>
+          <Heading as="h2" fontSize={[4, 5]} color="black" mb={[3, 5]}>
+            {getLocale(intl.locale).west.title}
+          </Heading>
+          {getLocale(intl.locale).west.bios.map(({ name, picture, title, bio }, index) => (
+            <Person key={index}>
+              <Flex wrap py={[7, 8]}>
+                <StyledBox width={[1 / 1, 1 / 4]} pr={[4, 6]}>
+                  <ImageContainer>
+                    {picture ?
+                      /* eslint-disable */
+                      <Img src={require(`./content/images/${picture}`)} alt={`Picture of ${name}`} /> : // eslint-disable-line global-require
+                      /* eslint-disable */
+                      <NoImg />
+                    }
+                  </ImageContainer>
+                </StyledBox>
+                <StyledBox width={[1 / 1, 3 / 4]}>
+                  <Heading heavy as="h2" fontSize={[3, 4]}>{name}</Heading>
+                  <Title>{title}</Title>
+                  <Bio>{bio}</Bio>
+                </StyledBox>
+              </Flex>
+            </Person>
+          ))}
+        </BioSection>
+        <BioSection>
+          <Heading as="h2" fontSize={[4, 5]} color="black" mb={[3, 5]}>
+            {getLocale(intl.locale).east.title}
+          </Heading>
+          {getLocale(intl.locale).east.bios.map(({ name, picture, title, bio }, index) => (
+            <Person key={index}>
+              <Flex wrap py={[7, 8]}>
+                <StyledBox width={[1 / 1, 1 / 4]} pr={[4, 6]}>
+                  <ImageContainer>
+                    {picture ?
+                      /* eslint-disable */
+                      <Img src={require(`./content/images/${picture}`)} alt={`Picture of ${name}`} /> : // eslint-disable-line global-require
+                      /* eslint-disable */
+                      <NoImg />
+                    }
+                  </ImageContainer>
+                </StyledBox>
+                <StyledBox width={[1 / 1, 3 / 4]}>
+                  <Heading heavy as="h2" fontSize={[3, 4]}>{name}</Heading>
+                  <Title>{title}</Title>
+                  <Bio>{bio}</Bio>
+                </StyledBox>
+              </Flex>
+            </Person>
+          ))}
+        </BioSection>
+        <BioSection>
+          <Heading as="h2" fontSize={[4, 5]} color="black" mb={[3, 5]}>
+            {getLocale(intl.locale).investors.title}
+          </Heading>
+          {getLocale(intl.locale).investors.bios.map(({ name, picture, title, bio }, index) => (
+            <Person key={index}>
+              <Flex wrap py={[7, 8]}>
+                <StyledBox width={[1 / 1, 1 / 4]} pr={[4, 6]}>
+                  <ImageContainer>
+                    {picture ?
+                      /* eslint-disable */
+                      <Img src={require(`./content/images/${picture}`)} alt={`Picture of ${name}`} /> : // eslint-disable-line global-require
+                      /* eslint-disable */
+                      <NoImg />
+                    }
+                  </ImageContainer>
+                </StyledBox>
+                <StyledBox width={[1 / 1, 3 / 4]}>
+                  <Heading heavy as="h2" fontSize={[3, 4]}>{name}</Heading>
+                  <Title>{title}</Title>
+                  <Bio>{bio}</Bio>
+                </StyledBox>
+              </Flex>
+            </Person>
+          ))}
+        </BioSection>
       </Wrapper>
     </Container>
     <Footer />
