1106957aa21fe9f311350a55a60e214142cf9d1b
Alex Sugak <aleksandr.sugak@gmail.com>
2018-03-21 17:04:10 +0200

    add 404 image

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -3,9 +3,12 @@ import PropTypes from 'prop-types';
 import { injectIntl, FormattedMessage } from 'react-intl';
 import styled from 'styled-components';
 import { Helmet } from 'react-helmet';
+import media from 'utils/media';
+import { rem } from 'polished';
 
 import Heading from 'components/Heading';
 import Button from 'components/Button';
+import notFound from './images/404.svg';
 
 const Wrapper = styled.div`
   text-align: center;
@@ -15,6 +18,18 @@ const Wrapper = styled.div`
   height: 100vh;
 `;
 
+const NotFoundImage = styled.img.attrs({
+  src: notFound,
+})`
+  display: block;
+  width: ${rem(300)};
+
+  ${media.sm.css`
+      border-top: none;
+      width: ${rem(600)};
+  `}
+`;
+
 const NotFound = ({ intl }) => (
   <Wrapper>
     <Helmet>
@@ -22,11 +37,13 @@ const NotFound = ({ intl }) => (
     </Helmet>
 
     <div>
+      <NotFoundImage />
+
       <Heading heavy as="h1" fontSize={[5, 6]} color="black" mb={[4, 6]}>
         <FormattedMessage id="notFound.heading" />
       </Heading>
 
-      <Button to="/" color="base" outlined big fontSize={[1, 3]}>
+      <Button to="/" color="white" bg="base" pill fontSize={[1, 3]}>
         <FormattedMessage id="notFound.home" />
       </Button>
     </div>
