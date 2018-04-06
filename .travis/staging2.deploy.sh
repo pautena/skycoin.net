b2f63467710578c0a37c3fed8d2096daa1ad6656
Alex Sugak <aleksandr.sugak@gmail.com>
2018-04-06 18:26:11 +0300

    add prod deploy script

--- .travis/staging2.deploy.sh
+++ .travis/staging2.deploy.sh
@@ -3,7 +3,7 @@ set -e
 
 aws configure list
 
-echo "sync build dir ($TRAVIS_BUILD_DIR/build) to s3 bucket ..."
+echo "sync build dir ($TRAVIS_BUILD_DIR/build) to s3://staging2.skycoin.net ..."
 aws s3 sync $TRAVIS_BUILD_DIR/build s3://staging2.skycoin.net --region ap-southeast-1 --delete --cache-control no-cache --acl public-read
 echo "done."
 
