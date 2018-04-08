255465f9176d7117ee7647b29bc151b171e0ac76
Alex Sugak <aleksandr.sugak@gmail.com>
2018-04-08 18:35:26 +0300

    set caching settings for static content

--- .travis/staging.deploy.sh
+++ .travis/staging.deploy.sh
@@ -6,8 +6,3 @@ aws configure list
 echo "sync build dir ($TRAVIS_BUILD_DIR/build) to s3://staging.skycoin.net ..."
 aws s3 sync $TRAVIS_BUILD_DIR/build s3://staging.skycoin.net --region ap-southeast-1 --delete --cache-control no-cache --acl public-read
 echo "done."
-
-echo "setting no cache for index.html ..."
-aws s3 cp s3://staging.skycoin.net/index.html s3://staging.skycoin.net/index.html --metadata-directive REPLACE --cache-control max-age=0,no-cache,no-store,must-revalidate --content-type text/html --acl public-read
-echo "done."
-
