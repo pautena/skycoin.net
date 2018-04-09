8ae9c88b2d7582d99efa4a83c5b38cd11bbdc98f
Kateryna Zviagina <apostatta@gmail.com>
2018-04-10 01:23:04 +0300

    downloads > fix typo

--- src/locales/en.js
+++ src/locales/en.js
@@ -284,7 +284,7 @@ export default {
     },
     bbs: {
       heading: 'Download Skycoin BBS',
-      lead: 'Skycoin BBS is a next generation decentralized social network built using Fiber, Skycoin\'s blockhain technology.',
+      lead: 'Skycoin BBS is a next generation decentralized social network built using Fiber, Skycoin\'s blockchain technology.',
       githublink: 'Find the latest binaries on Github.',
       thinclient: 'Access a BBS thin client.',
     },
