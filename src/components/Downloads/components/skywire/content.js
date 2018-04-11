a935e8d66cb1e3fcdb9960ea0bc5fd1f237a6404
Kateryna Zviagina <apostatta@gmail.com>
2018-04-11 19:34:19 +0300

    remove file

--- src/components/Downloads/components/skywire/content.js
+++
@@ -1,6 +0,0 @@
-export default [{
-  name: 'downloads.skywire.minerAssemblyManual',
-  download: 'https://downloads.skycoin.net/skywire/Skywire_Miner_Assembly_Manual_2_1.pdf',
-  filetype: '.pdf',
-  filesize: '5.1mb',
-}];
