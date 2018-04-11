c52ef9fc8384e87ec520b3880c4aa9310ba60382
Kateryna Zviagina <apostatta@gmail.com>
2018-04-11 19:36:30 +0300

    downloads > add skywire content file

---
+++ src/components/Downloads/components/Skywire/content.js
@@ -0,0 +1,6 @@
+export default [{
+  name: 'downloads.skywire.minerAssemblyManual',
+  download: 'https://downloads.skycoin.net/skywire/Skywire_Miner_Assembly_Manual_2_1.pdf',
+  filetype: '.pdf',
+  filesize: '5.1mb',
+}];
