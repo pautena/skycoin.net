a73f7caddfa906048c8510493710aaae11e159da
chalyi-andrei <andrei.chalyi@sigma.software>
2018-04-16 14:37:46 +0300

    Add Mandarin version of Skyminer assembly manual

--- src/components/Downloads/components/Skywire/content.js
+++ src/components/Downloads/components/Skywire/content.js
@@ -1,6 +1,14 @@
-export default [{
-  name: 'downloads.skywire.minerAssemblyManual',
-  download: 'https://downloads.skycoin.net/skywire/Skywire_Miner_Assembly_Manual_2_1.pdf',
-  filetype: '.pdf',
-  filesize: '5.1mb',
-}];
+export default [
+  {
+    name: 'downloads.skywire.minerAssemblyManual',
+    download: 'https://downloads.skycoin.net/skywire/Skywire_Miner_Assembly_Manual_2_1.pdf',
+    filetype: '.pdf',
+    filesize: '5.1mb',
+  },
+  {
+    name: 'Skyminer_矿机安装手册 v2.2',
+    download: '/Skyminer_v2.2.pdf',
+    filetype: '.pdf',
+    filesize: '7mb',
+  },
+];
