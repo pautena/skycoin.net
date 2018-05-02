becb28186dc8c5e7e3eb9479d9bb089a15fafcb2
chalyi-andrei <andrei.chalyi@sigma.software>
2018-05-02 12:48:26 +0300

    add obelisk icon to obelisk container

--- src/components/Ecosystem/content.js
+++ src/components/Ecosystem/content.js
@@ -4,7 +4,6 @@ export const skycoin = [
   { icon: images.fiber, label: 'ecosystem.iconLabel.fiber' },
   { icon: images.coinHours, label: 'ecosystem.iconLabel.coinHours' },
   { icon: images.cx, label: 'ecosystem.iconLabel.cx' },
-  { icon: images.obelisk, label: 'ecosystem.iconLabel.obelisk' },
   { icon: images.coinJoin, label: 'ecosystem.iconLabel.coinJoin' },
 ];
 
@@ -12,6 +11,10 @@ export const skywire = [
   { icon: images.skyminer, label: 'ecosystem.iconLabel.skyminer' },
   { icon: images.node, label: 'ecosystem.iconLabel.node' },
   { icon: images.cxo, label: 'ecosystem.iconLabel.cxo' },
+];
+
+export const obelisk = [
+  { icon: images.obelisk, label: 'ecosystem.iconLabel.obelisk' },
 ];
 
 export const experiments = [
