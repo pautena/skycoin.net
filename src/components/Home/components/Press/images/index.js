2b122be8a8a2f9d34db25f7379b84d5aba957ebc
Kateryna Zviagina <apostatta@gmail.com>
2018-05-21 15:51:46 +0300

    add links to press section

---
+++ src/components/Home/components/Press/images/index.js
@@ -0,0 +1,6 @@
+export { default as blockchain } from './icBlockchain.png';
+export { default as alphabit } from './icAlphabit.png';
+export { default as coinTelegraph } from './icCoinTelegrph.png';
+export { default as nasdaq } from './icNasdaq.png';
+export { default as forbes } from './icForbes.png';
+export { default as coinAgenda } from './icCionAgenda.png';
