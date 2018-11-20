753eee60a9de64c9bcac37d1f7e5f3e3d10bfc6f
Ivan Payan <ivanpayanm@gmail.com>
2018-11-20 11:11:11 +0100

    Lint fixes

--- src/components/Buy/Buy.js
+++ src/components/Buy/Buy.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 import styled from 'styled-components';
 
 import Button from '../Button';
-import {BUY_SKY_LINK} from "../../config";
+import { BUY_SKY_LINK } from '../../config';
 
 const Wrapper = styled.div`
   display: block;
