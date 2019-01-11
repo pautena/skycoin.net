3565b08444c72eb2d7ef6bb9227ce615c6ba2e29
Pau Tena Vidal <pautenavidal@gmail.com>
2019-01-11 11:52:08 +0100

    fixed lint problems

--- src/components/ThankYouPage/ThankYouPage.js
+++ src/components/ThankYouPage/ThankYouPage.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import { injectIntl, FormattedMessage } from 'react-intl';
 import { Helmet } from 'react-helmet';
 import { withRouter } from 'react-router';
@@ -9,29 +10,23 @@ import Footer from 'components/Footer';
 import FullScreenMessage from '../FullScreenMessage';
 
 const LOAD_DELAY = 500;
-const TIMEOUT_REDIRECT_DELAY =  LOAD_DELAY + 3000;
+const TIMEOUT_REDIRECT_DELAY = LOAD_DELAY + 3000;
 
-
-const ThankYouContent = () => {
-  return (
-    <div>
-      <Header />
-      <FullScreenMessage>
-        <FormattedMessage id="thankYou.message" />
-      </FullScreenMessage>
-      <Footer />
-    </div>
+const ThankYouContent = () =>
+  (<div>
+    <Header />
+    <FullScreenMessage>
+      <FormattedMessage id="thankYou.message" />
+    </FullScreenMessage>
+    <Footer />
+  </div>
   );
-}
+
 
 class ThankYouPage extends Component {
-
-  constructor(props){
+  constructor(props) {
     super(props);
-    this.state = {
-      loaded:false
-    }
-
+    this.state = { loaded: false };
   }
 
   componentDidMount() {
@@ -39,32 +34,38 @@ class ThankYouPage extends Component {
   }
 
   loading() {
-    setTimeout(()=>{
-      this.setState({loaded:true});
+    setTimeout(() => {
+      this.setState({ loaded: true });
       this.redirect();
-    },LOAD_DELAY);
+    }, LOAD_DELAY);
   }
 
-  redirect(){
-    setTimeout(()=>{
+  redirect() {
+    setTimeout(() => {
       this.redirectToHome();
-    },TIMEOUT_REDIRECT_DELAY);
-  }
-  
-  redirectToHome(){
-    this.props.history.push({pathname:'/'});
+    }, TIMEOUT_REDIRECT_DELAY);
   }
 
-  render(){
+  redirectToHome() {
+    this.props.history.push({ pathname: '/' });
+  }
+
+  render() {
     return (
       <div>
         <Helmet>
           <title>Thank you</title>
         </Helmet>
-        {this.state.loaded?<ThankYouContent />:null}
+        {this.state.loaded ? <ThankYouContent /> : null}
       </div>
     );
   }
 }
+
+ThankYouPage.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func.isRequired,
+  }),
+};
 
 export default withRouter(injectIntl(ThankYouPage));
