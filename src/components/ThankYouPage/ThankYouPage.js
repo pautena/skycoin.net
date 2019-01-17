84f374e242e893a554afd2455873a0a5b69d4dbe
Pau Tena Vidal <pautenavidal@gmail.com>
2019-01-17 16:05:59 +0100

    fixed thank you page

--- src/components/ThankYouPage/ThankYouPage.js
+++ src/components/ThankYouPage/ThankYouPage.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component, PureComponent } from 'react';
 import PropTypes from 'prop-types';
 import { injectIntl, FormattedMessage } from 'react-intl';
 import { Helmet } from 'react-helmet';
@@ -11,32 +11,45 @@ import FullScreenMessage from '../FullScreenMessage';
 const TIMEOUT_REDIRECT_DELAY = 3000;
 
 
-const redirect = (history) => {
-  setTimeout(() => {
-    history.push({ pathname: '/' });
-  }, TIMEOUT_REDIRECT_DELAY);
-};
+class ThankYouPage extends PureComponent {
+  constructor(props) {
+    super(props);
+  }
 
-const ThankYouPage = ({ history }) => {
-  redirect(history);
-  return (
-    <div>
-      <Helmet>
-        <title>Thank you</title>
-      </Helmet>
-      <Header />
-      <FullScreenMessage>
-        <FormattedMessage id="thankYou.message" />
-      </FullScreenMessage>
-      <Footer />
-    </div>
-  );
-};
+  componentDidMount() {
+    this.redirect();
+  }
+
+  redirect() {
+    setTimeout(() => {
+      this.redirectToHome();
+    }, TIMEOUT_REDIRECT_DELAY);
+  }
+
+  redirectToHome() {
+    this.props.history.push({ pathname: '/' });
+  }
+
+  render() {
+    return (
+      <div>
+        <Helmet>
+          <title>Thank you</title>
+        </Helmet>
+        <Header />
+        <FullScreenMessage>
+          <FormattedMessage id="thankYou.message" />
+        </FullScreenMessage>
+        <Footer />
+      </div>
+    );
+  }
+}
 
 ThankYouPage.propTypes = {
   history: PropTypes.shape({
     push: PropTypes.func.isRequired,
-  }).isRequired,
+  }),
 };
 
 export default injectIntl(ThankYouPage);
